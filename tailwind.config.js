e9e81b3ce7afcf20d7bebc888cfdc99b34f0787f
20jun <ex_xe@naver.com>
2022-04-06 14:57:04 +0900

    #3.1 TailwindCSS Setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    // Tailwind를 어디서 사용 할건지
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
