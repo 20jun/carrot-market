acbd8681d6036bd81a66c99341e45828604b8cba
20jun <ex_xe@naver.com>
2022-05-19 23:35:59 +0900

    #22.1 Indexes and Deploys

---
+++ pages/coins.server.tsx
@@ -0,0 +1,60 @@
+import { Suspense } from "react";
+
+const cache: any = {};
+
+function fetchData(url: string) {
+  if (!cache[url]) {
+    throw fetch(url)
+      .then((r) => r.json())
+      .then((json) => (cache[url] = json));
+  }
+  return cache[url];
+}
+
+function Coin({ id, name, symbol }: any) {
+  const {
+    quotes: {
+      USD: { price },
+    },
+  } = fetchData(`https://api.coinpaprika.com/v1/tickers/${id}`);
+  console.log(price);
+  return (
+    <span>
+      {name} / {symbol}: ${price}
+    </span>
+  );
+}
+
+function List() {
+  const coins = fetchData("https://api.coinpaprika.com/v1/coins");
+
+  return (
+    <div>
+      <h4>List is done</h4>
+      <ul>
+        {coins.slice(0, 10).map((coin: any) => (
+          <li key={coin.id}>
+            <Suspense key={coin.id} fallback={`Coin ${coin.name} is loading`}>
+              <Coin key={coin.id} {...coin} />
+            </Suspense>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default function Coins() {
+  return (
+    <div>
+      <h1>Welcome to RSC</h1>
+      <Suspense fallback="Rendering in the server...">
+        <List />
+      </Suspense>
+    </div>
+  );
+}
+
+export const config = {
+  runtime: "edge",
+};
